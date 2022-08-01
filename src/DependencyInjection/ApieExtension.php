7141ed83d163d3ac154564150bd1c95307ab0639
Pieter <pieter_jordaan@hotmail.com>
2022-08-01 14:19:09 +0000

    update codestyle + readme etc.

---
+++ src/DependencyInjection/ApieExtension.php
@@ -0,0 +1,39 @@
+<?php
+namespace Apie\ApieBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ApieExtension extends Extension
+{
+    private array $dependencies = [
+        'enable_rest_api' => [
+            'rest_api.yaml',
+            'schema_generator.yaml',
+            'serializer.yaml',
+        ]
+    ];
+
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../resources/config'));
+        $loader->load('services.yaml');
+        $loader->load('psr7.yaml');
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+        $container->setParameter('apie.bounded_contexts', $config['bounded_contexts']);
+        $loaded = [];
+        foreach ($this->dependencies as $configName => $dependencyList) {
+            if ($config[$configName]) {
+                foreach ($dependencyList as $dependency) {
+                    if (!isset($loaded[$dependency])) {
+                        $loaded[$dependency] = true;
+                        $loader->load($dependency);
+                    }
+                }
+            }
+        }
+    }
+}
